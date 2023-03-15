3fbcc82b03985d77116092a7415e15a2d8c61605
rafal <plaszczyk.com@gmail.com>
2023-03-15 20:56:20 +0100

    add chatMessageService test suite

--- src/services/chatMessageService/chatMessageService.spec.js
+++ src/services/chatMessageService/chatMessageService.spec.js
@@ -1,3 +1,19 @@
-describe('the Chat Message Service', () => {
-  it.todo('should return an array of messages');
+import { ChatMessageService } from "./";
+describe("the Chat Message Service", () => {
+  let chatMessageService;
+
+  beforeEach(() => {
+    chatMessageService = ChatMessageService([]);
+  });
+  it("should add messages", async () => {
+    const content = "blabla";
+    expect(await chatMessageService.getMessages()).toHaveLength(0);
+
+    await chatMessageService.submitMessage({ content });
+
+    expect(await chatMessageService.getMessages()).toHaveLength(1);
+
+    let messages = await chatMessageService.getMessages();
+    expect(messages[0].content).toEqual(content);
+  });
 });
