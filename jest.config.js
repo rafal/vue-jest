58617df049a094b02ca40a37a9bb40068df85fe4
rafal <plaszczyk.com@gmail.com>
2023-03-15 19:45:31 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  moduleFileExtensions: ["js", "vue"],
+  moduleFileExtensions: ["js", "json", "vue"],
   transform: {
       '^.+\\.vue$': 'vue-jest',
-      '^.+\\.(js|jsx)?$': 'babel-jest'
+      '^.+\\.(js|jsx)?$': 'babel-jest',
+      // process `*.vue` files with `vue-jest`
+      ".*\\.(vue)$": "vue-jest"
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/"]
 }
